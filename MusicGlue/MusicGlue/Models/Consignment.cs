d52c8a07322e81b2e884d80bf1c47329f7b645f8
Janick Rasmussen <itsjoohneey@gmail.com>
2026-04-15 10:19:43 +0200

    Consignment and MusicProduct added.

---
+++ MusicGlue/MusicGlue/Models/Consignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGlue.Models
+{
+    public class Consignment
+    {
+        public List<MusicProduct> MusicProducts { get; set; }
+        public int Id { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string CustomerCountry { get; set; }
+
+        public ConsignmentStatus ConsignmentStatus { get; set; }
+
+        public bool ReportingStatus { get; set; }
+    }
+
+    public enum ConsignmentStatus
+    {
+        NotDispatched,
+        PreOrdered,
+        Dispatched
+    }
+}
---
+++ MusicGlue/MusicGlue/Models/MusicProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGlue.Models
+{
+    public class MusicProduct
+    {
+        public int ProductId { get; set; }
+
+        public double Price { get; set; }
+    }
+}
