c5897a209a3a8ace421a9213692facf1cd3d99b8
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-04-15 09:41:57 +0200

    VS project init

---
+++ MusicGlue/MusicGlue/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MusicGlue
+{
+    internal static class Configuration
+    {
+        private static readonly IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        public static string ConnectionString { get; } = configuration.GetConnectionString("MyDBConnection") ?? "";
+    }
+}
