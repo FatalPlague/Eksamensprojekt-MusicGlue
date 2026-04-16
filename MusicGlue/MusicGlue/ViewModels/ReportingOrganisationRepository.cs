6119d14d341f2bdaabd47efdf192e179c94c30c8
Janick Rasmussen <itsjoohneey@gmail.com>
2026-04-16 10:04:49 +0200

    Starting reportingorg

---
+++ MusicGlue/MusicGlue/ViewModels/ReportingOrganisationRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace MusicGlue.ViewModels
+{
+    public class ReportingOrganisationRepository
+    {
+        private readonly string ConnectionString;
+        private List<ReportingOrganisation> reportingOrganisations;
+
+        public ReportingOrganisationRepository()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            cars = new List<Car>();
+
+            ConnectionString = config.GetConnectionString("MyDBConnection");
+        }
+
+        // Relevante CRUD-metoder indsættes her
+
+    }
+}
