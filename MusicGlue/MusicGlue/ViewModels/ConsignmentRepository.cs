ef9e5350d7da9c843a698ddc1b09c3ef0f7c8c22
Sara Hougaard <saraolsen20@gmail.com>
2026-04-21 11:33:58 +0200

    update and getbycustomercountry methods added

--- MusicGlue/MusicGlue/ViewModels/ConsignmentRepository.cs
+++ MusicGlue/MusicGlue/ViewModels/ConsignmentRepository.cs
@@ -3,6 +3,7 @@ using MusicGlue.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
 
 namespace MusicGlue.ViewModels
 {
@@ -25,8 +26,41 @@ namespace MusicGlue.ViewModels
             {
                 con.Open();
 
-                string query = "";
+                string query = "SELECT CONSIGNMENT.Id, CONSIGNMENT.CustomerCountry, CONSIGNMENT.ZipCode, CONSIGNMENT.Consignmentstatus, CONSIGNMENT.ReportingStatus, MUSICPRODUCT.Id, MUSICPRODUCT.Price, MUSICPRODUCT.ProductDescriptionId " +
+                    "FROM CONSIGNMENT, MUSICPRODUCT_CONSIGNMENT, MUSICPRODUCT " +
+                    "WHERE Consignment.Id = MUSICPRODUCT_CONSIGNMENT.ConsignmentId AND MUSICPRODUCT.Id = MUSICPRODUCT_CONSIGNMENT.ProductId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+
+                }
             }
+        }
+
+        public void Update (Consignment consignmentToBeUpdated)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "UPDATE CONSIGNMENT " +
+                    "SET CONSIGNMENT.ReportingStatus = @CONSIGNMENT.ReportingStatus " +
+                    "WHERE Id = @Id";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.Add("@CONSIGNMENT.ReportingStatus", SqlDbType.Bit).Value = consignmentToBeUpdated.ReportingStatus;
+                    cmd.Parameters.Add("@Id", SqlDbType.Int).Value = consignmentToBeUpdated.Id;
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Consignment> GetByCustomerCountry(string country)
+        {
+            return consignments.FindAll(o => o.CustomerCountry == country);
         }
     }
 }
