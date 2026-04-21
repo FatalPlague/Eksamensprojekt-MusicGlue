66455eb09a62a6985d440910b102765679d9204b
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-04-21 09:50:26 +0200

    ConsignmentRepo started

---
+++ MusicGlue/MusicGlue/ViewModels/ConsignmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using MusicGlue.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGlue.ViewModels
+{
+    public class ConsignmentRepository
+    {
+        private readonly string connectionString;
+        private List<Consignment> consignments;
+
+        public ConsignmentRepository()
+        {
+            connectionString = Configuration.ConnectionString;
+
+            consignments = new List<Consignment>();
+            InitializeRepository();
+        }
+
+        private void InitializeRepository()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "";
+            }
+        }
+    }
+}
