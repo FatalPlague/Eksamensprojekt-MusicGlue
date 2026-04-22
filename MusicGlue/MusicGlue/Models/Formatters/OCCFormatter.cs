63a949e55be5b85da63689c3385889096a009e25
Mathias Sørensen <mathiassrensen514@gmail.com>
2026-04-22 11:45:09 +0200

    Start on Formatter

---
+++ MusicGlue/MusicGlue/Models/Formatters/OCCFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGlue.Models.Formatters
+{
+    public class OCCFormatter : IFormatter
+    {
+        public string Format()
+        {
+            return string.Empty;
+        }
+    }
+}
