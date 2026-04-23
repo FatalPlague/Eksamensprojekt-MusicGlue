aa719103dfe544c2294e61e93e5294af04cba360
Mathias Sørensen <mathiassrensen514@gmail.com>
2026-04-23 13:43:32 +0200

    Formatter

--- MusicGlue/MusicGlue/Models/Formatters/OCCFormatter.cs
+++ MusicGlue/MusicGlue/Models/Formatters/OCCFormatter.cs
@@ -8,20 +8,86 @@ namespace MusicGlue.Models.Formatters
     {
         public string Format(List<Consignment> consignments)
         {
-            //Header line
-            string zipCode = "0" + consignments[0].ZipCode;
-            string date = DateTime.Now.ToString("YYMMDD");
-            
-            //
-            foreach (MusicProduct musicProduct in consignments[0].MusicProducts)
+            string date = DateTime.Now.ToString("yyMMdd");
+            List<string> strings = new List<string>();
+
+            List<string> zipcodes = consignments
+                .Select(consignment => consignment.ZipCode)
+                .Distinct()
+                .ToList();
+
+            zipcodes.ForEach(zipcode =>
             {
+                int count = 0;
+                string zipcodePaddet = zipcode.PadRight(5, ' ');
+                strings.Add("0" + zipcodePaddet + date + Environment.NewLine);
+                
 
-            }
-            
+                Dictionary<int, List<MusicProduct>> musicProductsByZipcode = GetMusicProductsByZipcode(consignments, zipcode);
+                musicProductsByZipcode.Keys.ToList().ForEach(musicProductId =>
+                {
+                    Dictionary<double, List<MusicProduct>> musicProductsByPrice = GetMusicProductsByPrice(musicProductsByZipcode[musicProductId]);
+                    musicProductsByPrice.Keys.ToList().ForEach(price =>
+                    {
+                        List<MusicProduct> musicProducts = musicProductsByPrice[price].OrderBy(x => x.Price).ToList();
+                        string amount = musicProducts.Count.ToString().PadLeft(6, '0');
+                        string barcode = musicProducts[0].Description.Barcode;
+                        string priceString = (price * 100).ToString().PadLeft(5, '0');
 
-            //footer
+                        strings.Add("1" + barcode + amount + " " + priceString + Environment.NewLine);
+                        count++;
+                    });
+                });
 
-            return string.Empty;
+                string amount = count.ToString().PadLeft(5, '0');
+                strings.Add("9" + zipcodePaddet + amount);
+
+                if (zipcodes.IndexOf(zipcode) != zipcodes.Count - 1)
+                {
+                    strings.Add(Environment.NewLine + Environment.NewLine);
+                }
+            });
+
+            return string.Join("", strings);
+        }
+
+        private Dictionary<int, List<MusicProduct>> GetMusicProductsByZipcode(List<Consignment> consignments, string zipcode)
+        {
+            Dictionary<int, List<MusicProduct>> musicProducts = new Dictionary<int, List<MusicProduct>>();
+            List<Consignment> consignmentsByZip = consignments
+                .Where(consignment => consignment.ZipCode == zipcode)
+                .ToList();
+
+            consignmentsByZip.ForEach(consignment =>
+            {
+                consignment.MusicProducts.ForEach(musicProduct =>
+                {
+                    if (!musicProducts.ContainsKey(musicProduct.Id))
+                    {
+                        musicProducts[musicProduct.Id] = new List<MusicProduct>();
+                    }
+
+                    musicProducts[musicProduct.Id].Add(musicProduct);
+                });
+            });
+
+            return musicProducts;
+        }
+
+        private Dictionary<double, List<MusicProduct>> GetMusicProductsByPrice(List<MusicProduct> musicProducts)
+        {
+            Dictionary<double, List<MusicProduct>> musicProductsByPrice = new Dictionary<double, List<MusicProduct>>();
+            musicProducts.ForEach(musicProduct =>
+            {
+                if (!musicProductsByPrice.ContainsKey(musicProduct.Price))
+                {
+                    musicProductsByPrice[musicProduct.Price] = new List<MusicProduct>();
+                }
+
+                musicProductsByPrice[musicProduct.Price].Add(musicProduct);
+            });
+
+            return musicProductsByPrice;
         }
     }
 }
--- MusicGlue/MusicGlueTest/Test1.cs
+++ MusicGlue/MusicGlueTest/Test1.cs
@@ -8,8 +8,10 @@ namespace MusicGlueTest
     public sealed class Test1
     {
         // Arrange
+        string date = DateTime.Now.ToString("yyMMdd"); 
         List<Consignment> consignments = new List<Consignment>();
         IFormatter formatter;
+
 
         [TestInitialize]
         public void init()
@@ -18,26 +20,34 @@ namespace MusicGlueTest
             ProductDescription d1 = new ProductDescription()
             {
                 Id = 1,
-                Barcode = "521900114742",
+                Barcode = "5321900114742",
             };
 
             ProductDescription d2 = new ProductDescription()
             {
                 Id = 1,
-                Barcode = "856200115725",
+                Barcode = "5321900983034",
             };
 
             MusicProduct m1 = new MusicProduct()
             {
                 Id = 1,
-                Price = 1.56,
+                Price = 5.99,
                 Description = d1
             };
+
 
             MusicProduct m2 = new MusicProduct()
             {
                 Id = 2,
-                Price = 13.60,
+                Price = 4.99,
+                Description = d1
+            };
+
+            MusicProduct m3 = new MusicProduct()
+            {
+                Id = 3,
+                Price = 12.99,
                 Description = d2
             };
 
@@ -48,24 +58,64 @@ namespace MusicGlueTest
                 CustomerCountry = "England",
                 ConsignmentStatus = ConsignmentStatus.Dispatched,
                 ReportingStatus = false,
-                MusicProducts = new List<MusicProduct>() {m1, m1, m2, m2, m2, m2 ,m2 ,m2 ,m2 ,m2 ,m2 ,m2 }
+                MusicProducts = new List<MusicProduct>() { m1, m1, m1, m2, m2, m3, m3 }
+            };
+
+            Consignment c2 = new Consignment()
+            {
+                Id = 2,
+                ZipCode = "WC",
+                CustomerCountry = "England",
+                ConsignmentStatus = ConsignmentStatus.Dispatched,
+                ReportingStatus = false,
+                MusicProducts = new List<MusicProduct>() { m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m3, m2, m1, m1, m1 }
+            };
+
+            Consignment c3 = new Consignment()
+            {
+                Id = 3,
+                ZipCode = "L",
+                CustomerCountry = "England",
+                ConsignmentStatus = ConsignmentStatus.Dispatched,
+                ReportingStatus = false,
+                MusicProducts = new List<MusicProduct>() { m1, m1, m3 }
             };
 
             consignments.Add(c1);
+            consignments.Add(c2);
+            consignments.Add(c3);
             formatter = new OCCFormatter();
         }
 
         [TestMethod]
         public void OCCFormatterTest()
         {
+
+            string actual = formatter.Format(consignments);
+            string expected = "0WC   " + date + Environment.NewLine +
+                "15321900114742000006 00599" + Environment.NewLine +
+                "15321900114742000003 00499" + Environment.NewLine +
+                "15321900983034000022 01299" + Environment.NewLine +
+                "9WC   00003" + Environment.NewLine +
+                Environment.NewLine +
+                "0L    " + date + Environment.NewLine +
+                "15321900114742000002 00599" + Environment.NewLine +
+                "15321900983034000001 01299" + Environment.NewLine +
+                "9L    00002";
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Expected:");
+            Console.WriteLine(expected);
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("-------------------");
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Actual:");
+            Console.WriteLine(actual);
+
             //Assert
             Assert.AreEqual(
-                "0WC  260423" + Environment.NewLine +
-                "1521900114742000002 00156" + Environment.NewLine +
-                "1856200115725000010 01360" + Environment.NewLine +
-                "9WC  00002",
-
-                formatter.Format(consignments)
+                expected,
+                actual
             );
         }
     }
