fdd246e6b0d1ae8c3b8c7c6c015e339a80640241
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-04-27 09:18:33 +0200

    HomeViewModel created

---
+++ MusicGlue/MusicGlue/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGlue.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string ScriptRunStatus { get; set; }
+
+        public HomeViewModel(string scriptRunStatus)
+        {
+            ScriptRunStatus = scriptRunStatus;
+        }
+
+    }
+}
