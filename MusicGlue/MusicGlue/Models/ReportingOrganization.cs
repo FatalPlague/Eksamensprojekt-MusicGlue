9ebfe1474b43b081aa6ccee640b99ead48e55b3b
Natasha Nordentoft <n.nordentoft@outlook.com>
2026-04-30 11:44:58 +0200

    Deleted non-necessary attributes from ReportingOrganisation

--- MusicGlue/MusicGlue/Models/ReportingOrganization.cs
+++ MusicGlue/MusicGlue/Models/ReportingOrganization.cs
@@ -9,8 +9,6 @@ namespace MusicGlue.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
-        public string Format { get; set; }
-        public string IdentificationType { get; set; }
         public IFormatter Formatter { get; set; }
     }
 }
